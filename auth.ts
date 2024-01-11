481e6370884bcb5d23d99bb60c2edfc60de46163
Ron Almog <ron8132@gmail.com>
2024-01-11 16:47:12 -0500

    add role to session

--- auth.ts
+++ auth.ts
@@ -3,6 +3,7 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 import authConfig from "@/auth.config";
 import { db } from "./lib/db";
+import { getUserById } from "./data/user";
 
 const prisma = new PrismaClient();
 
@@ -16,12 +17,25 @@ export const {
     // looks like session is receivin the token from jwt callback (below)
     // so we can pass extra params into the session thru the token
     async session({ token, session }) {
+      console.log({ sessionToken: token });
       if (token.sub && session.user) {
         session.user.id = token.sub;
       }
+
+      if (token.role && session.user) {
+        session.user.role = token.role;
+      }
+
       return session;
     },
     async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+
       console.log(token);
       return token;
     },
