795461fa9c78e5c477516cab6e8efbf620e90cef
Ron Almog <ron8132@gmail.com>
2024-01-11 12:08:15 -0500

    add test settings page

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,11 +1,26 @@
 // server component
-import { auth } from "@/auth";
-import { json } from "stream/consumers";
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
 
 const SettingsPage = async () => {
   const session = await auth();
 
-  return <div>{JSON.stringify(session)}</div>;
+  return (
+    <div>
+      {JSON.stringify(session)}
+
+      <form
+        action={async () => {
+          "use server";
+          await signOut();
+        }}
+      >
+        <Button type="submit" variant="outline">
+          Sign Out
+        </Button>
+      </form>
+    </div>
+  );
 };
 
 export default SettingsPage;
