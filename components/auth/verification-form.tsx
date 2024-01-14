afef8215f65dff4da6350cfb5242f743489ced2d
Ron Almog <ron8132@gmail.com>
2024-01-13 19:34:06 -0500

    complete email verification

--- components/auth/verification-form.tsx
+++ components/auth/verification-form.tsx
@@ -2,10 +2,42 @@
 
 import { BeatLoader } from "react-spinners";
 import CardWrapper from "./card-wrapper";
+import { useSearchParams } from "next/navigation";
+import { useCallback, useEffect, useState } from "react";
+import { verification } from "@/actions/verification";
+import FormSuccess from "../form-success";
+import FormError from "../form-error";
 
 type Props = {};
 
 const VerificationForm = (props: Props) => {
+  const [error, setError] = useState<string | undefined>();
+  const [success, setSuccess] = useState<string | undefined>();
+  const searchParams = useSearchParams();
+  const token = searchParams.get("token");
+
+  const onSubmit = useCallback(() => {
+    // to solve the issue of calling the useEffect twice in dev
+    if (success || error) return;
+
+    if (!token) {
+      setError("Missing token!");
+      return;
+    }
+    verification(token)
+      .then((data) => {
+        setSuccess(data.success);
+        setError(data.error);
+      })
+      .catch(() => {
+        setError("Something went wrong!");
+      });
+  }, [token, success, error]);
+
+  useEffect(() => {
+    onSubmit();
+  }, [onSubmit]);
+
   return (
     <CardWrapper
       headerLabel="Confirming your verification"
@@ -13,7 +45,9 @@ const VerificationForm = (props: Props) => {
       backButtonLabel="Back to login"
     >
       <div className="w-full flex items-center justify-center">
-        <BeatLoader />
+        {!success && !error && <BeatLoader />}
+        <FormSuccess message={success} />
+        <FormError message={error} />
       </div>
     </CardWrapper>
   );
