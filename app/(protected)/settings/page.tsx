dd5d64d81ba96c79ecb27ae3399d790999f06245
Ron Almog <ron8132@gmail.com>
2024-01-15 14:34:16 -0500

    client side useSession

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,24 +1,22 @@
-// server component
-import { auth, signOut } from "@/auth";
-import { Button } from "@/components/ui/button";
+"use client";
 
-const SettingsPage = async () => {
-  const session = await auth();
+import { Button } from "@/components/ui/button";
+import { useSession, signOut } from "next-auth/react";
+
+const SettingsPage = () => {
+  const session = useSession();
+
+  const onClick = () => {
+    signOut();
+  };
 
   return (
     <div>
       {JSON.stringify(session)}
 
-      <form
-        action={async () => {
-          "use server";
-          await signOut();
-        }}
-      >
-        <Button type="submit" variant="outline">
-          Sign Out
-        </Button>
-      </form>
+      <Button onClick={onClick} variant="outline">
+        Sign Out
+      </Button>
     </div>
   );
 };
