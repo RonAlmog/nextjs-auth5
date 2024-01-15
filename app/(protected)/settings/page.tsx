479d4cb7401a40637fa31417f395933e1dfedae9
Ron Almog <ron8132@gmail.com>
2024-01-15 14:38:48 -0500

    getCurrentUser client side hook

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { useCurrentUser } from "@/hooks/use-current-user";
 import { useSession, signOut } from "next-auth/react";
 
 const SettingsPage = () => {
-  const session = useSession();
+  // const session = useSession(); // whole session object
+  const user = useCurrentUser(); // only user object from session (usually enough)
 
   const onClick = () => {
     signOut();
@@ -12,7 +14,7 @@ const SettingsPage = () => {
 
   return (
     <div>
-      {JSON.stringify(session)}
+      {JSON.stringify(user)}
 
       <Button onClick={onClick} variant="outline">
         Sign Out
