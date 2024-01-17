f25658e8a946b42fc210e661a6c75b591b7c39eb
Ron Almog <ron8132@gmail.com>
2024-01-17 09:09:11 -0500

    admins only server action

--- app/(protected)/admin/page.tsx
+++ app/(protected)/admin/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { admin } from "@/actions/admin";
 import { RoleGate } from "@/components/auth/role-gate";
 import FormSuccess from "@/components/form-success";
 import { Button } from "@/components/ui/button";
@@ -25,6 +26,17 @@ const AdminPage = () => {
     });
   };
 
+  const onServerActionClick = () => {
+    admin().then((data) => {
+      if (data.success) {
+        toast.success(data.success);
+      }
+      if (data.error) {
+        toast.error(data.error);
+      }
+    });
+  };
+
   return (
     <Card className="w-[600px]">
       <CardHeader>
@@ -40,7 +52,7 @@ const AdminPage = () => {
         </div>
         <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
           <p className="text-sm font-medium">Admin only Server Action</p>
-          <Button>Click to test</Button>
+          <Button onClick={onServerActionClick}>Click to test</Button>
         </div>
       </CardContent>
     </Card>
