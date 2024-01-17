e67c876fc6b623cff51f2880112f757bf694094f
Ron Almog <ron8132@gmail.com>
2024-01-16 19:24:09 -0500

    add role-gate

--- app/(protected)/admin/page.tsx
+++ app/(protected)/admin/page.tsx
@@ -1,15 +1,39 @@
-// "use clinet";
+"use client";
 
+import { RoleGate } from "@/components/auth/role-gate";
+import FormSuccess from "@/components/form-success";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { useCurrentRole } from "@/hooks/use-current-role";
-import { currentRole } from "@/lib/auth";
+import { UserRole } from "@prisma/client";
+// import { currentRole } from "@/lib/auth";
 
-const AdminPage = async () => {
+const AdminPage = () => {
   // client
-  // const role = useCurrentRole();
+  const role = useCurrentRole();
   // server
-  const role = await currentRole();
+  // const role = await currentRole();
 
-  return <div>current role: {role}</div>;
+  return (
+    <Card className="w-[600px]">
+      <CardHeader>
+        <p className="text-2xl font-semibold text-center">Admin</p>
+      </CardHeader>
+      <CardContent className="space-y-4">
+        <RoleGate allowedRole={UserRole.ADMIN}>
+          <FormSuccess message="Hey Admin, you can see this!" />
+        </RoleGate>
+        <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
+          <p className="text-sm font-medium">Admin only API Route</p>
+          <Button>Click to test</Button>
+        </div>
+        <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
+          <p className="text-sm font-medium">Admin only Server Action</p>
+          <Button>Click to test</Button>
+        </div>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default AdminPage;
